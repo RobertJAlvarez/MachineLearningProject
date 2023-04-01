f3eed09cef112eb9e98cc16df4304ffbe2422c12
RobertJAlvarez <robert.alvarez644@gmail.com>
2023-04-01 05:16:06 -0600

    Structure started for future algorithm implementation

---
+++ EnsembleClassifier.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+class EnsembleClassifier:
+  def __init__(self):
+    #
+    pass
+
+  def fit(self):
+    #
+    pass
+
+  def predict(self):
+    #
+    pass
+
+if __name__ == '__main__':
+  print()
+
---
+++ EnsembleRegressor.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+class EnsembleRegressor:
+  def __init__(self):
+    #
+    pass
+
+  def fit(self):
+    #
+    pass
+
+  def predict(self):
+    #
+    pass
+
+if __name__ == '__main__':
+  print()
+
