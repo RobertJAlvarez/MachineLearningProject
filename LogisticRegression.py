faa3f9fc24171f35d33132cb728d96623f84ccca
RobertJAlvarez <robert.alvarez644@gmail.com>
2023-04-08 20:26:49 -0600

    Vectorization added for one-hot representation

--- LogisticRegression.py
+++ LogisticRegression.py
@@ -2,6 +2,7 @@ import numpy as np
 from PerformanceMetrics import accuracy
 from DataSets import process_gamma_dataset
 from sklearn.linear_model import LogisticRegression as LR
+from time import time
 
 def standardize(x):
   for i in range(x.shape[1]):
@@ -16,29 +17,25 @@ class LogisticRegression:
     self.tol = tol
     self.max_iter = max_iter
 
-  #TODO: Upgrade this so it can handle one-hot representations
   def fit(self, x_train, y_train, alpha=0.001):
     X = np.c_[np.ones((x_train.shape[0],1)),x_train]  #Add a column of 1's at the beginning of the data to calculate y intercept
     betas = np.zeros((X.shape[1],1))  #We have as many betas as we have attributes
-    y = np.reshape(y_train,(-1,1))      #Reshape y_train so it is a 1 column matrix
+    y = np.reshape(y_train,(-1,1))    #Reshape y_train so it is a 1 column matrix
     for _ in range(self.max_iter):
       p = self.sigmoid(np.dot(X,betas))
       W = p*(1-p)
-      temp2 = W*X
       temp = np.linalg.inv(np.matmul(X.T, W*X))
       new_betas = betas + np.matmul(np.matmul(temp,X.T),(y-p))
       if np.sum(abs(new_betas-betas)) < self.tol: break
       betas = new_betas
-    self.betas = betas
+    self.intercept_ = betas[0,0]
+    self.coef_ = betas[1:,0]
 
-  #TODO: Upgrade this so it can handle one-hot representations
   def predict(self, x_test):
     X = np.c_[np.ones((x_test.shape[0],1)),x_test]
-    z = np.dot(X, self.betas)
-    pred = np.empty(x_test.shape[0])
-    for i,val in enumerate(self.sigmoid(z)):
-      pred[i] = 1 if val>0.5 else 0
-    return pred
+    betas = np.hstack((np.reshape(self.intercept_,(-1)),self.coef_))
+    z = np.dot(X,betas)
+    return (self.sigmoid(z)>0.5).astype(int)
 
   def __str__(self):
     return '{}(max_iter={:03}, penalty={})'.format(self.__class__.__name__,self.max_iter,self.penalty)
@@ -50,17 +47,21 @@ if __name__ == '__main__':
 
   #My model
   print("My model")
+  t0 = time()
   model = LogisticRegression()
   print("model = ", model)
   model.fit(x_train, y_train)
   pred = model.predict(x_test)
-  print("accuracy: ", accuracy(y_test, pred))
+  print("Elapse time = {:.3f}".format(time() - t0))
+  print("accuracy: {:.5f}".format(accuracy(y_test, pred)))
 
   #Run sklearn simple model
   print("\nSKLEARN model")
+  t0 = time()
   model = LR(max_iter=100, penalty=None, tol=1e-4)
   print("model = ", model)
   model.fit(x_train, y_train)
   pred = model.predict(x_test)
-  print("accuracy: ", accuracy(y_test, pred))
+  print("Elapse time = {:.5f}".format(time() - t0))
+  print("accuracy: {:.5f}".format(accuracy(y_test, pred)))
 
