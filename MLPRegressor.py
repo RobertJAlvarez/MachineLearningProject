31e5a1be3dc4fce91378a46baab62eac48076836
RobertJAlvarez <robert.alvarez644@gmail.com>
2023-04-17 00:25:43 -0600

    Fitting executes succesfully but testing needs to be done

--- MLPRegressor.py
+++ MLPRegressor.py
@@ -26,16 +26,17 @@ class MLPRegressor:
     batch_size = min(200, x_train.shape[0])
     n_batches = y_train.shape[0]//batch_size
 
-    Z = [0]*len(self.coef_)
-    A = [0]*len(self.coef_)
-    dZ = [0]*len(self.coef_)
-    dW = [0]*len(self.coef_)
-    db = [0]*len(self.coef_)
     for i_batch in range(n_batches):
+      Z = [0]*len(self.coef_)
+      A = [0]*len(self.coef_)
+      dZ = [0]*len(self.coef_)
+      dW = [0]*len(self.coef_)
+      db = [0]*len(self.coef_)
+
       #Subset x_train and y_train for this batch
       start_row = i_batch*batch_size
       sub_x = x_train[start_row:start_row+batch_size]
-      sub_y = y_train[start_row:start_row+batch_size]
+      sub_y = y_train[start_row:start_row+batch_size].reshape((-1,1))
 
       #Forward propagation
       Z[0] = np.matmul(sub_x,self.coef_[0]) + self.intercepts_[0]
@@ -47,22 +48,31 @@ class MLPRegressor:
       #Backward propagation
       i = len(Z)-1
       dZ[i] = A[i] - sub_y
-      dW[i] = np.matmul(dZ[i],A[i])/batch_size
+      dW[i] = np.matmul(A[i-1].T,dZ[i],)/batch_size
       db[i] = np.sum(dZ[i],axis=1,keepdims=True)/batch_size
-      for i in range(len(Z)-2,-1,-1):
-        dZ[i] = np.matmul(np.matmul(dW[i+1].T,dZ[i+1]),relu(Z[i]))
+      for i in range(len(Z)-2,0,-1):
+        dZ[i] = np.matmul(dW[i+1].T,dZ[i+1]) * relu(Z[i])
         dW[i] = np.matmul(dZ[i],A[i].T)/batch_size
         db[i] = np.sum(dZ[i],axis=1,keepdims=True)/batch_size
+      dZ[0] = np.matmul(dZ[1],dW[1].T) * relu(Z[0])
+      dW[0] = np.matmul(sub_x.T,dZ[0])/batch_size
+      db = np.sum(dZ[0],axis=1,keepdims=True)/batch_size
 
       #Update weights and biases
       for i in range(len(Z)):
-        print("self.coef_[i].shape = ", self.coef_[i].shape)
-        print("dW[i].shape = ", dW[i].shape)
         self.coef_[i] -= self.lern_rate*dW[i]
         self.intercepts_[i] -= self.lern_rate*db[i]
 
+    #for i in range(len(self.coef_)):
+    #  print("self.coef_[i].shape = ", self.coef_[i].shape)
+    #  print("self.intercepts_[i].shape = ", self.intercepts_[i].shape)
+
   def predict(self, x_test):
-    return
+    H = np.matmul(x_test,self.coef_[0]) + self.intercepts_[0]
+    for i in range(1,len(self.coef_)):
+      H = relu(H)
+      H = np.matmul(H,self.coef_[i]) + self.intercepts_[i]
+    return H
 
 if __name__ == '__main__':
   x_train,x_test,y_train,y_test = process_gpu_running_time()
@@ -71,8 +81,8 @@ if __name__ == '__main__':
   t0 = time()
   model = MLPRegressor()
   model.fit(x_train,y_train)
-  #print("MSE = ", MSE(y_test, model.predict(x_test)))
-  #print("Elapse time = {:.5f}".format(time() - t0))
+  print("MSE = ", MSE(y_test, model.predict(x_test)))
+  print("Elapse time = {:.5f}".format(time() - t0))
 
   #print("\nSKLEARN model:")
   #t0 = time()
