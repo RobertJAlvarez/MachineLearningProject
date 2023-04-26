be01bfb6eb17a374e13c28892977abe20f14f442
RobertJAlvarez <robert.alvarez644@gmail.com>
2023-04-25 18:20:23 -0600

    Sigmoid funciton added

--- Utils.py
+++ Utils.py
@@ -3,10 +3,8 @@ import numpy as np
 def to_categorical(y):
   y_vals = np.unique(y)
   cat_y = np.empty(shape=(y.shape[0],y_vals.shape[0]))
-
   for i,y_val in enumerate(y_vals):
     cat_y[:,i] = (y==y_val).astype(int)
-
   return cat_y
 
 def standardize(x):
@@ -46,6 +44,9 @@ class LabelEncoder:
       temp[i==data] = c
     return temp
 
-def relu(z):
-  return np.maximum(np.array([[0]]),z)
+def relu(x):
+  return np.maximum(np.array([[0]]),x)
+
+def sigmoid(x):
+  return 1./(1.+exp(-x))
 
